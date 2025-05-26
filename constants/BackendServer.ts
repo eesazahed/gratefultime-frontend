10cb87db4d74ef39a04bd9ccf068f76f3bf8e982
Eesa Zahed <eesa.zahed@gmail.com>
2025-05-26 12:49:37 -0400

    test

--- constants/BackendServer.ts
+++ constants/BackendServer.ts
@@ -1,3 +1,5 @@
 const DEV_MODE = false;
 
-export const BackendServer = DEV_MODE ? "http://10.0.0.15:5000/api/v1" : "";
+export const BackendServer = DEV_MODE
+  ? "http://10.0.0.15:5000/api/v1"
+  : "https://www.gratefultime.app/api/v1";
